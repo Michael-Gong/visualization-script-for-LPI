17c513fed0e7529daa5df1f64301e76f1292a329
Michael Gong <zgong@pku.edu.cn>
2018-05-28 22:03:52 -0700

    Add files via upload

--- wrap_energy_spectra_nonrelativistic.py
+++ wrap_energy_spectra_nonrelativistic.py
@@ -52,12 +52,12 @@ if __name__ == "__main__":
 
 
   def pxpy_to_energy(gamma, weight):
-      binsize = 500
-      en_grid = np.linspace(0.5,499.5,binsize)
-      en_bin  = np.linspace(0,500.0,binsize+1)
+      binsize = 300
+      en_grid = np.linspace(0.5,599.5,binsize)
+      en_bin  = np.linspace(0,600.0,binsize+1)
       en_value = np.zeros_like(en_grid) 
       for i in range(binsize):
-        en_value[i] = np.sum(weight[ (en_bin[i]<=gamma) & (gamma<en_bin[i+1]) ])/(500.0/binsize)
+        en_value[i] = sum(weight[ (en_bin[i]<=gamma) & (gamma<en_bin[i+1]) ])/(600.0/binsize)
       return (en_grid, en_value)
 
   to_path='./'
@@ -82,76 +82,52 @@ if __name__ == "__main__":
   V=(1.0/20.0)*(1.0/15.0)*(1.0/15.0)*1.0e-18
   weight = V*denunit*n0/50.0 
 
-  set_relativistic =1 
+  set_relativistic =0 
   for n in np.arange(15,16):
     for m in np.arange(27,28): 
       data = sdf.read("./i_tot_loc"+str(n).zfill(4)+".sdf",dict=True)
       header=data['Header']
       time=header['time']
-      if set_relativistic == 1:
-          px = data['Particles/Px/subset_Only_Ions0/Ion'].data/(1836*m0*v0)
-          py = data['Particles/Py/subset_Only_Ions0/Ion'].data/(1836*m0*v0)
-          pz = data['Particles/Pz/subset_Only_Ions0/Ion'].data/(1836*m0*v0)
-          gg = (px**2+py**2+py**2+1)**0.5
-          theta = np.arctan2((py**2+pz**2)**0.5,px)*180.0/np.pi
-    
-          ek_1 = (gg - 1.0)*0.51*1836
-          ww_1 = np.zeros_like(ek_1) + weight
-          dist_x1, den1 = pxpy_to_energy(ek_1,ww_1)
-    
-          ek_2 = (gg[abs(theta)<10.0] - 1.0)*0.51*1836
-          ww_2 = np.zeros_like(ek_2) + weight
-          dist_x2, den2 = pxpy_to_energy(ek_2,ww_2)
-          print('set_relativistic 1'+str(n).zfill(4))
-      elif set_relativistic == 0: 
+      if set_relativistic == 0: 
           px = data['Particles/Px/subset_Only_Ions0/Ion'].data
           py = data['Particles/Py/subset_Only_Ions0/Ion'].data
           pz = data['Particles/Pz/subset_Only_Ions0/Ion'].data
           pp = (px**2+py**2+py**2)**0.5
           theta = np.arctan2((py**2+pz**2)**0.5,px)*180.0/np.pi
-    
+          print('max theta is ',theta.max(),theta.min()) 
+        
           ek_1 = pp**2/2.0/1836/m0/(1.6e-13)
+          print('maximum ek_1 is',ek_1.max())
           ww_1 = np.zeros_like(ek_1) + weight
           dist_x1, den1 = pxpy_to_energy(ek_1,ww_1)
     
           ek_2 = (pp[abs(theta)<10.0])**2/2.0/1836/m0/(1.6e-13)
+          print('maximum ek_2 is',ek_2.max())
           ww_2 = np.zeros_like(ek_2) + weight
           dist_x2, den2 = pxpy_to_energy(ek_2,ww_2)
-          print('set_relativistic 0'+str(n).zfill(4))
+          print('set_relativistic 0;'+str(n).zfill(4))
            
       data = sdf.read("./i_tot_loc"+str(m).zfill(4)+".sdf",dict=True)
       header=data['Header']
       time=header['time']
-      if set_relativistic == 1:
-          px = data['Particles/Px/subset_Only_Ions0/Ion'].data/(1836*m0*v0)
-          py = data['Particles/Py/subset_Only_Ions0/Ion'].data/(1836*m0*v0)
-          pz = data['Particles/Py/subset_Only_Ions0/Ion'].data/(1836*m0*v0)
-          gg = (px**2+py**2+py**2+1)**0.5
-          theta = np.arctan2((py**2+pz**2)**0.5,px)*180.0/np.pi
-    
-          ek_3 = (gg - 1.0)*0.51*1836
-          ww_3 = np.zeros_like(ek_3) + weight
-          dist_x3, den3 = pxpy_to_energy(ek_3,ww_3)
-    
-          ek_4 = (gg[abs(theta)<10.0] - 1.0)*0.51*1836
-          ww_4 = np.zeros_like(ek_4) + weight
-          dist_x4, den4 = pxpy_to_energy(ek_4,ww_4)
-          print('set_relativistic 1'+str(m).zfill(4))
-      elif set_relativistic == 0:
+      if set_relativistic == 0:
           px = data['Particles/Px/subset_Only_Ions0/Ion'].data
           py = data['Particles/Py/subset_Only_Ions0/Ion'].data
-          pz = data['Particles/Py/subset_Only_Ions0/Ion'].data
+          pz = data['Particles/Pz/subset_Only_Ions0/Ion'].data
           pp = (px**2+py**2+py**2)**0.5
           theta = np.arctan2((py**2+pz**2)**0.5,px)*180.0/np.pi
+          print('max theta is ',theta.max(),theta.min()) 
     
           ek_3 = pp**2/2.0/1836/m0/(1.6e-13)
+          print('maximum ek_3 is',ek_3.max())
           ww_3 = np.zeros_like(ek_3) + weight
           dist_x3, den3 = pxpy_to_energy(ek_3,ww_3)
     
           ek_4 = (pp[abs(theta)<10.0])**2/2.0/1836/m0/(1.6e-13)
+          print('maximum ek_4 is',ek_4.max())
           ww_4 = np.zeros_like(ek_4) + weight
           dist_x4, den4 = pxpy_to_energy(ek_4,ww_4)
-          print('set_relativistic 0'+str(m).zfill(4))
+          print('set_relativistic 0;'+str(m).zfill(4))
           
     
       plt.plot(dist_x1,den1,':b',linewidth=4, label='before; total')
@@ -165,7 +141,7 @@ if __name__ == "__main__":
       plt.xticks(fontsize=20); plt.yticks(fontsize=20);
       plt.yscale('log')
       plt.ylim(2e7,8e9)
-      plt.xlim(5,500)
+      plt.xlim(10,600)
       plt.grid(which='major',color='k', linestyle='--', linewidth=0.3)
       plt.grid(which='minor',color='k', linestyle='--', linewidth=0.1)
 
@@ -174,7 +150,7 @@ if __name__ == "__main__":
                 wspace=None, hspace=None)
     #        plt.text(250,6e9,'t='+str(round(time/1.0e-15,0))+' fs',fontdict=font)
       fig = plt.gcf()
-      fig.set_size_inches(8.4, 7.0)
-      fig.savefig('./relativistic_maybe'+str(n).zfill(4)+'_'+str(m).zfill(4)+'.png',format='png',dpi=160)
+      fig.set_size_inches(10.0, 6.0)
+      fig.savefig('./maybe_nonrelativistic_'+str(n).zfill(4)+'_'+str(m).zfill(4)+'.png',format='png',dpi=160)
       plt.close("all")
       print('finished!'+str(n).zfill(4)+'_'+str(m).zfill(4))      
